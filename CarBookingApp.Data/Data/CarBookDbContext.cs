f19eed9833dd0fa115000773fc86fe946a27f89a
Vikingslord <ahsanulhaque1998@outlook.com>
2023-02-09 13:43:22 +0600

    Add project files.

---
+++ CarBookingApp.Data/Data/CarBookDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CarBookingApp.Data.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarBookingApp.Data.Data
+{
+    public class CarBookDbContext : DbContext //Represents Database
+    {
+        public CarBookDbContext(DbContextOptions<CarBookDbContext> options) : base(options)
+        { 
+
+        }
+         
+        public DbSet<Car> Cars { get; set; }  // Database Table
+
+    }  
+}
---
+++ CarBookingApp/Pages/Cars/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CarBookingApp.Data.Data;
+using CarBookingApp.Data.Model;
+
+namespace CarBookingApp.Pages.Cars
+{
+    public class IndexModel : PageModel
+    {
+        private readonly CarBookingApp.Data.Data.CarBookDbContext _context;
+
+        public IndexModel(CarBookingApp.Data.Data.CarBookDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Car> Car { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Cars != null)
+            {
+                Car = await _context.Cars.ToListAsync();
+            }
+            else
+            {
+                NotFound();
+            }
+        }
+    }
+}
